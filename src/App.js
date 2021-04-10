f0eb94bc923e3e16ae732e0ccf2745756374b460
Salih Can <hasansalihcan@gmail.com>
2021-04-10 09:27:00 +0300

    Change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload hihihi.
+          Edit <code>src/App.js</code> and save to (changed in feature branch).
         </p>
         <a
           className="App-link"
